fb0bf0784b8d8bdb1f34fbe7d3bd877729e68dab
Aymen El Arrouji <aymane.arrouji@gmail.com>
2026-03-10 01:37:42 +0000

    hello

--- 09/ex02/PmergeMe.cpp
+++ 09/ex02/PmergeMe.cpp
@@ -16,7 +16,6 @@ PmergeMe& PmergeMe::operator=(const PmergeMe& other)
     {
         this->d = other.d;
         this->v = other.v;
-        this->nums = other.nums;
         this->straggler = other.straggler;
     }
     return *this;
@@ -31,7 +30,31 @@ bool isduplicate(const std::vector<int>& nums, int num)
     return std::find(nums.begin(), nums.end(), num) != nums.end();
 }
 
-void PmergeMe::parse_input(int argc, char **argv)
+void PmergeMe::make_pairs(std::vector <int> winners, std::vector <int> losers, std::vector <int> nums)
+{
+    for (size_t i = 0; i < nums.size(); i += 2)
+    {
+        if (i + 1 < nums.size())
+        {
+            if (nums[i] < nums[i + 1])
+            {
+                winners.push_back(nums[i + 1]);
+                losers.push_back(nums[i]);
+            }
+            else
+            {
+                winners.push_back(nums[i]);
+                losers.push_back(nums[i + 1]);
+            }
+        }
+        else
+        {
+            straggler = nums[i];
+        }
+    }
+}
+
+void PmergeMe::parse_input(int argc, char **argv, std::vector <int> &nums)
 {
     char *end;
     long long tmp;
@@ -45,4 +68,71 @@ void PmergeMe::parse_input(int argc, char **argv)
         nums.push_back(tmp);
     }
     this->straggler = -1;
+}
+
+void PmergeMe::sort_vec(std::vector <int> nums)
+{
+    if (nums.size() == 2)
+    {
+        if (nums[0] > nums[1])
+        {
+            v.push_back(nums[1]);
+            v.push_back(nums[0]);
+        }
+        else
+        {
+            v.push_back(nums[0]);
+            v.push_back(nums[1]);
+        }
+        return ;
+    }
+    std::vector <int> winners;
+    std::vector <int> losers;
+    make_pairs(winners, losers, nums);
+    sort_vec(winners);
+    merge_losers_vec(losers);
+}
+
+void PmergeMe::sort_deque(std::vector <int> nums)
+{
+    if (nums.size() == 2)
+    {
+        if (nums[0] > nums[1])
+        {
+            d.push_back(nums[1]);
+            d.push_back(nums[0]);
+        }
+        else
+        {
+            d.push_back(nums[0]);
+            d.push_back(nums[1]);
+        }
+        return ;
+    }
+    std::vector <int> winners;
+    std::vector <int> losers;
+    make_pairs(winners, losers, nums);
+    sort_deque(winners);
+    merge_losers_deque(losers);
+}
+
+std::vector<int> PmergeMe::jacobsthal_seq()
+{
+}
+
+void PmergeMe::merge_losers_vec(std::vector <int> losers)
+{
+}
+
+void PmergeMe::merge_losers_deque(std::vector <int> losers)
+{ 
+}
+
+void PmergeMe::sort(int argc, char **argv)
+{
+    std::vector <int> nums;
+
+    parse_input(argc, argv, nums);
+    sort_vec(nums);
+    sort_deque(nums);
 }
--- 09/ex02/PmergeMe.hpp
+++ 09/ex02/PmergeMe.hpp
@@ -13,10 +13,16 @@ class PmergeMe
     private:
         std::deque  <int> d;
         std::vector <int> v;
-        std::vector <int> nums;
         int         straggler;
-        void        parse_input(int argc, char **argv);
 
+        void                parse_input(int argc, char **argv, std::vector <int> &nums);
+        void                make_pairs(std::vector <int> winners, std::vector <int> losers, std::vector <int> nums);
+        void                sort_vec(std::vector <int> nums);
+        void                sort_deque(std::vector <int> nums);
+        void                merge_losers_vec(std::vector <int> losers);
+        void                merge_losers_deque(std::vector <int> losers);
+        void                sort(int argc, char **argv);
+        std::vector<int>    jacobsthal_seq();
     public:
         PmergeMe();
         PmergeMe(const PmergeMe& other);
